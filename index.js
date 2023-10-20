47d04a82126ec2db5f344e05e1feee2a3e4dfa93
t.vinh25112003 <t.vinh25112003@gmail.com>
2023-10-20 13:07:46 +0700

    ud cors

--- index.js
+++ index.js
@@ -5,34 +5,18 @@ const WebRouter = require('./src/routers/web');
 const apirouter = require('./src/routers/api');
 const fileUpload = require('express-fileupload');
 const connection = require('./src/config/database');
+const cors = require('cors');
+
 const {
     default: mongoose
 } = require('mongoose');
 const app = express();
 const port = process.env.PORT || 8000;
 const hostname = process.env.HOST_NAME;
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500/');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-//confix template engine
 //config req.body
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({
     extended: true
 }));
